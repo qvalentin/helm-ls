9705534b994aba81f168f2d766d1187c6ae44881
qvalentin <valentin.theodor@web.de>
2023-11-05 15:13:30 +0100

    WIP: Improve publish time of yamlls diagnostics

--- internal/lsp/diagnostics.go
+++ internal/lsp/diagnostics.go
@@ -6,9 +6,11 @@ import (
 )
 
 type diagnosticsCache struct {
-	YamlDiagnostics []lsp.Diagnostic
-	HelmDiagnostics []lsp.Diagnostic
-	helmlsConfig    *util.HelmlsConfiguration
+	YamlDiagnostics             []lsp.Diagnostic
+	HelmDiagnostics             []lsp.Diagnostic
+	helmlsConfig                *util.HelmlsConfiguration
+	gotYamlDiagnosticsTimes     int
+	yamlDiagnosticsCountReduced bool
 }
 
 func NewDiagnosticsCache(helmlsConfig *util.HelmlsConfiguration) diagnosticsCache {
@@ -16,7 +18,15 @@ func NewDiagnosticsCache(helmlsConfig *util.HelmlsConfiguration) diagnosticsCach
 		[]lsp.Diagnostic{},
 		[]lsp.Diagnostic{},
 		helmlsConfig,
+		0,
+		false,
 	}
+}
+
+func (d diagnosticsCache) SetYamlDiagnostics(diagnostics []lsp.Diagnostic) {
+	d.yamlDiagnosticsCountReduced = len(diagnostics) < len(d.YamlDiagnostics)
+	d.YamlDiagnostics = diagnostics
+	d.gotYamlDiagnosticsTimes++
 }
 
 func (d diagnosticsCache) GetMergedDiagnostics() (merged []lsp.Diagnostic) {
@@ -30,4 +40,11 @@ func (d diagnosticsCache) GetMergedDiagnostics() (merged []lsp.Diagnostic) {
 		}
 	}
 	return merged
+}
+
+func (d diagnosticsCache) ShouldShowDiagnosticsOnNewYamlDiagnostics() bool {
+
+	return d.yamlDiagnosticsCountReduced || // show the diagnostics when the count is reduced, this means an error was fixed and it should be shown to the user
+		d.helmlsConfig.YamllsConfiguration.ShowDiagnosticsDirectly || // show the diagnostics directly when the user configured to show them
+		d.gotYamlDiagnosticsTimes < 2 // show the diagnostics, when it are the inital diagnostics that are sent after opening a file. Initial diagnostics are sent twice from yamlls
 }
